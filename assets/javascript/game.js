50022f3b37e3712e000f986940722b6e95fff09f
Nenye <chinenyediei@gmail.com>
2019-04-20 01:29:42 -0500

    late night changes, reformated and refactored

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,58 +1,108 @@
+//Global variables
 
-
-var wordsArray = ["50 Cent", "Eminem", "Ludacris", "Nick Jonas", "Lady Gaga", "Kanye West", "Joyner Lucas", "Rick Ross", "Halsey", "Iggy Azalea"];
-var guessArray = [""];
-var guessesLeft = 15;
+var wordsArray = ["Juvenile", "Biggie Smalls", "Eminem", "Ludacris", "Nick Jonas", "Lady Gaga", "Kanye West", "Joyner Lucas", "Rick Ross", "Halsey", "Iggy Azalea", "Elvis Presley", "Whitney Houston", "sia"];
+var wrongLettersArray = []; //empty araay to store users wrong guesses 
+var guessesLeft = 9; //stores number of guesses remaining
+var lettersArray = []; //holds word to be guessed
+var currentWordArray = [];//holds _ _ _ _ _
 var wins = 0;
+var losses = 0;
+var userGuess = "";
 var winsText = document.getElementById("wins-text");
 var currentText = document.getElementById("currentWord-text");
 var guessesText = document.getElementById("guesses-text");
 var lettersText = document.getElementById("lettersGuessed-text");
+var lossesText = document.getElementById("losses-text");
+var gameWord = "";
+var noSpaces = "";
+var blankSpaces = 0;
 
+//functions
 
-document.onkeyup = function (event) {
+function startGame() {
 
+    //gets randomword from array
+    gameWord = wordsArray[Math.floor(Math.random() * wordsArray.length)];
 
-    var userGuess = event.key;
-    alert("you pressed the " + userGuess + " key.");
-
-    //random number between 0 and 9 0
-    var randomWord = Math.floor(Math.random() * wordsArray.length);
-    alert(randomWord);
-
-
-    //tells us the position in array of random number computer selected
-
-    var guessBank = "";
-    var gameWord = (wordsArray[randomWord]);
+    //converts word to lowercase and removes spaces
     gameWord = gameWord.toLowerCase();
-    alert(gameWord);
-    var noSpaces = gameWord.replace(/\s+/g, '');
-    alert(noSpaces);
-    alert(noSpaces.length);
+    noSpaces = gameWord.replace(/\s+/g, '');
+    lettersArray = noSpaces.split("");
+    blankSpaces = noSpaces.length;
+
+
+
+    //Reset
+    guessesText = 9;
+    currentWordArray = [];
+    wrongLettersArray = [];
+
+    //loop to create _ _ _ _ 
+    for (var i = 0; i < blankSpaces; i++) {
+        currentWordArray.push("_");
+    }
+
+    //push content to HTML
+    currentText.innerHTML = currentWordArray.join(" ");
+    winsText.innerHTML = wins;
+    lossesText.innerHTML = losses;
+    guessArray.push(userGuess);
+    guessesText.textContent = guessesLeft;
+
+
+
+
+    //testing & debugging
+    console.log(lettersArray);
+    console.log(gameWord);
+    console.log(noSpaces);
+    console.log(noSpaces.length);
+    console.log(currentWordArray);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     // create _ _ _ _ 
 
 
+    // if (guessesLeft <= 0) {
+    //     alert("Game Over");
+    // }
 
- 
-    // currentText.textContent = str; 
+    // for (var i = 0; i < noSpaces.length; i++) {
+
+
+    //     var indices = [];
+    //     for (var i = 0; i < noSpaces.length; i++) {
+    //         if (gameWord[i] === userGuess) indices.push(i);
+    //         alert(indices); 
+    //     }
+
+
 
 
     //make sure user still has guesses left
-    if (guessesLeft <= 0) {
-        alert("Game Over");
-    }
 
-    for (var i = 0; i < noSpaces.length; i++) {
-        
-        if (gameWord(i).indexOf(userGuess) > 0) {
-            alert("You guessed a correct letter");
 
-            //reduce guesses and push guesses left to HTML
-            guessesLeft--;
-            alert("You have " + guessesLeft + " guesses left");
-            guessesText.textContent = guessesLeft; 
+
+    // } {
+    //     alert("You guessed a correct letter");
+
+    //     //reduce guesses and push guesses left to HTML
+    //     guessesLeft--;
+    //     alert("You have " + guessesLeft + " guesses left");
 
 
 
@@ -61,24 +111,38 @@ document.onkeyup = function (event) {
 
 
 
-        }
-        else {
-            alert("You missed ");
-            guessesLeft--;
-            alert("You have " + guessesLeft + " guesses left");
-            guessesText.textContent = guessesLeft; 
-        }
-    }
+
+    // }
+    // else {
+    //     alert("You missed ");
+    //     guessesLeft--;
+    //     alert("You have " + guessesLeft + " guesses left");
+
+    // }
+
     //check userguess vs letters in user guess
 
 
- 
-
-    //
 
 
 
 
 
 
-    };
+
+
+}
+
+//Main process
+startGame();
+
+document.onkeyup = function (event) {
+    //gets key pushed from user
+    var userGuess = event.key;
+
+    //testing 
+    console.log(userGuess);
+
+
+}
+
